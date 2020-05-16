ae6f8b550b6851967e44db809127e97870655805
Joseph <example@gmail.com>
2020-05-16 05:10:55 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+
+"Use strict";
+
+
+
+const answers = [];
+
+answers[0] = prompt('kak vashe imya', '');
+answers[1] = prompt('kak vashe familiya', '');
+answers[2] = prompt('kak vashe otchestvo', '');
+
+console.log(typeof(answers));
