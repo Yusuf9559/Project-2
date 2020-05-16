e18948e5681158effeff8109d8db2e3ebb3989a9
Joseph <example.gamil.com>
2020-05-16 09:03:10 +0300

    no good

---
+++ js-2.js
@@ -0,0 +1,35 @@
+"Use strict";
+
+// if (4 == 9) {
+//     console.log("ok!");
+// } else {
+//     console.log('Error');
+// }
+
+
+// const num = 50;
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log('mnogo');
+// } else {
+//     console.log('ok!');
+// }
+
+// (num === 50) ? console.log('ok!') : console.log('Error');
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log("neverno");
+        break;
+    case 100:    
+        console.log("neverno");
+        break;
+    case 51:
+        console.log("verno");
+        break;
+    default:
+        console.log("ne v etot raz");
+        break;        
+}
--- js.js
+++ js.js
@@ -3,10 +3,17 @@
 
 
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('kak vashe imya', '');
-answers[1] = prompt('kak vashe familiya', '');
-answers[2] = prompt('kak vashe otchestvo', '');
+// answers[0] = prompt('kak vashe imya', '');
+// answers[1] = prompt('kak vashe familiya', '');
+// answers[2] = prompt('kak vashe otchestvo', '');
 
-console.log(typeof(answers));
+// console.log(typeof(answers));
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;    
